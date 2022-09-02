740e18e58eb7c54bfb2ef3c1c5d6039bd45cf6c8
clickurtrip <it@clickurtrip.com>
2022-09-02 11:01:13 +0530

    Properly Arrange and Separate by using different Component for differnt work

--- src/ApiFetch.jsx
+++ src/ApiFetch.jsx
@@ -1,54 +1,30 @@
 import React from "react";
-import { useState, useEffect } from "react";
 import "./cart.css"
-import { GiShoppingCart } from "react-icons/gi";
-import { Routes, Route, Link, } from "react-router-dom"
-import Cart from "./Cart";
-
-
-
-function ApiFetch() {
-
-    ///// Below Line No 14-15 indicate the state which are going to change
-
-    const [products, getProducts] = useState([])
-    const [cart, setCart] = useState([])
-
-
-    //// Below useEffect function For Fetching the Api From Server (Line No 20 to 26)
-
-    useEffect(() => {
-        fetch("https://fakestoreapi.com/products")
-            .then((Response) => Response.json())
-            .then((data) => {
-                getProducts(data)
-            })
-    }, [])
+import { FaShoppingCart } from "react-icons/fa";
+import { Link, } from "react-router-dom"
 
 
 
 
+function ApiFetch({ products, addeditems }) {
 
+
+    // console.log(products);
 
     return (<div id="mainCart">
 
-        {/* Below carIconDiv is to displace the cart icon on topside of bowser (I used nmp installation for react icon installing  fron React Icon website) */}
+
 
         <div id="cartIconDiv" ><h1 id="webSiteName">Shopping Website</h1>
-            {/* Below use of Link to Link the cart icon to next "Cart" Page where items is added */}
             <Link to="/Cart">
-                <h1 id="cartIcon"><GiShoppingCart /></h1>
+                <h1 id="cartIcon"><FaShoppingCart /></h1>
             </Link>
 
-            {/* Below Use of Routes and Route to connect the "Cart" page and i am given the Cart call element a props to acesse the added items in "Cart Page"
- --> look  this part {<Cart car={cart}/>}  */}
-            <Routes> <Route path="Cart" element={<Cart cart={cart} />} /></Routes>
 
 
         </div>
-        <div id="mapDiv">{
-            // use of map to render all products line No 50 to 75
-            products.map((product) => {
+        <div id="mapDiv">
+            {products.map((product) => {
                 return (
                     <div id="parentDiv" key={product.id}>
                         <h4 id="h4Id">Product No: {product.id}</h4>
@@ -68,14 +44,14 @@ function ApiFetch() {
                                 </li>
                             </ul>
                             <div>
-                                {/* below button on click set the select product to Your state"in my code selected items store to {cart} " */}
-                                <button id="btn1" onClick={() => setCart((prevState) => [...prevState, product])}>Add</button>
+
+                                <button id="btn1" onClick={() => addeditems(product)}>Add</button>
                             </div>
                         </div>
                     </div >
                 )
             })
-        }
+            }
         </div>
     </div>)
 
--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 
-import React from "react"
-import { BrowserRouter, } from "react-router-dom";
+import React, { useEffect, useState } from "react"
+import { Route, Routes } from "react-router-dom";
+
 
 import ApiFetch from "./ApiFetch";
-
-
+import Cart from "./Cart"
 // import Clickcounting from "./Clickcounting";
 
 
+
+
 function App() {
-  return (
-    <>
-
-      {/* <Clickcounting /> */}
-      <BrowserRouter>
-        <ApiFetch />
-        {/* <Routes> <Route path="Cart" /></Routes> */}
-      </BrowserRouter>
+  const [products, getProducts] = useState([])
+  const [cart, setCart] = useState([])
+  // console.log("......................");
+  // console.log(products);
 
 
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((Response) => Response.json())
+      .then((data) => {
+        getProducts(data)
+      })
+  }, [])
 
-    </>
+  const addeditems = (product) => {
+    // console.log(product);
+    setCart((prevState) => [...prevState, product])
+  }
+
+
+
+
+  return (<>
+    <Routes>
+      <Route path="/" element={<ApiFetch products={products} addeditems={addeditems} />} />
+      <Route path="cart" element={<Cart cart={cart} />} />
+    </Routes>
+  </>
+
   );
 }
 
